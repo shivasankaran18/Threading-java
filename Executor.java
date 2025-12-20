45037f3b2969a02d755a18404af4aa2ba99c5eaf
shiva <shiva_05@outlook.com>
2025-12-20 17:08:48 +0530

    wait-and-notify

--- Executor.java
+++ Executor.java
@@ -4,10 +4,10 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-class Thread implements Callable<Integer> {
+class Sum implements Callable<Integer> {
    int no;
 
-   public Thread(int no) {
+   public Sum(int no) {
       this.no = no;
    }
 
@@ -22,21 +22,32 @@ class Thread implements Callable<Integer> {
 
 }
 
-public class Thread1
+class Demo implements Callable<Void> {
+   @Override
+   public Void call() throws InterruptedException{
+      Thread.sleep(10000);
+      return null;
+   }
+
+}
+
+public class Executor
 {
    public static void main(String[] args) throws ExecutionException, InterruptedException {
       ExecutorService executor = Executors.newFixedThreadPool(3);
 
-      Callable<Integer> worker1 = new Thread(50000000);
-      Callable<Integer> worker2 = new Thread(10000000);
-      Callable<Integer> worker3 = new Thread(15000000);
-
+      Callable<Integer> worker1 = new Sum(10);
+      Callable<Void> worker2 = new Demo();
+      Callable<Integer> worker3 = new Sum(20);
+      
       Future<Integer> result1 = executor.submit(worker1);
-      Future<Integer> result2 = executor.submit(worker2);
+      Future<Void> result2 = executor.submit(worker2);
       Future<Integer> result3 = executor.submit(worker3);
-
+   
       System.out.println("Sum of first 5 natural numbers: " + result1.get());
-      System.out.println("Sum of first 10 natural numbers: " + result2.get());
+
+      //this is a blocking call . The main thread will wait here until the result is available
+      System.out.println(result2.get());
       System.out.println("Sum of first 15 natural numbers: " + result3.get());
 
       executor.shutdown();
---
+++ WaitAndNotify.java
@@ -0,0 +1,78 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class Shared
+{
+
+   synchronized void waitingCall()
+   {
+      System.out.println("Waiting call started "+Thread.currentThread().getName());
+      try
+      {
+         wait();
+      }
+      catch(InterruptedException e)
+      {
+         System.out.println("Waiting call interrupted");
+      }
+      System.out.println("Notified"+Thread.currentThread().getName());
+   }
+
+   synchronized void notifyingCall()
+   {
+      System.out.println("Notifying call started "+Thread.currentThread().getName());
+      notify();
+      System.out.println("Notifying call ended "+Thread.currentThread().getName());
+   }
+
+}
+
+
+
+public class WaitAndNotify
+{
+   public static void main(String args[]) throws InterruptedException
+   {
+      Shared shared=new Shared();
+
+      ExecutorService executor=Executors.newFixedThreadPool(4);
+
+      Runnable r1=()->{
+         shared.waitingCall();
+      };
+
+      Runnable r2=()->{
+         try
+         {
+            Thread.sleep(2000);
+         }
+         catch(InterruptedException e)
+         {
+            System.out.println("Thread interrupted");
+         }
+         shared.waitingCall();
+      };
+      Runnable r3=()->{
+         try
+         {
+            Thread.sleep(4000);
+         }
+         catch(InterruptedException e)
+         {
+            System.out.println("Thread interrupted");
+         }
+         shared.notifyingCall();
+      };
+
+      executor.submit(r1);
+      executor.submit(r2);
+      executor.submit(r3);
+      executor.submit(r3);
+      executor.shutdown();
+
+
+
+
+   }
+      
+}
